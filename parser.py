ba44ab83bcf59700ae3545c99f23114374930877
MichaelRayven <themikerayven@gmail.com>
2022-02-14 01:59:28 +0700

    Used multithreading when fetching html docs

--- parser.py
+++ parser.py
@@ -1,22 +1,33 @@
 import json
 import re
 import requests
+import threading
 from bs4 import BeautifulSoup
 
 
 class Parser:
     def __init__(self, url_list: list) -> None:
-        self.urls = url_list
+        self._urls = url_list
 
     def parse_data(self) -> list:
-        data = []
-        for url in self.urls:
-            (status, html) = self.__fetch_url(url)
-            if not status: continue
-            (status, data) = self.__get_page_data(html)
-            if not status: continue
-            data.append(data)
-        return data
+        self._data = []
+        threads = []
+        for url in self._urls:
+            thread = threading.Thread(target=self.__process_url, args=[url])
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+            
+        print("[INFO] Parsing finished")
+        return self._data
+
+    def __process_url(self, url):
+        status, html = self.__fetch_url(url)
+        if not status: return
+        status, data = self.__get_page_data(html)
+        if not status: return
+        self._data.append(data)
 
     def __get_page_data(self, html_doc):
         soup = BeautifulSoup(html_doc, "html.parser")
@@ -24,10 +35,10 @@ class Parser:
         if script_tags is None or len(script_tags) < 2: return (False, None)
         
         script_text = script_tags[1].getText()
-        match = re.search(r"\"data\":(\[.*\])", script_text, re.MULTILINE)
+        match = re.search(r"\"data\":(\[.*\])", script_text, flags=re.MULTILINE|re.DOTALL)
         if match is None: return (False, None)
         
-        data = json.loads(match.group())
+        data = json.loads(match.group(1))
         return (True, data)
 
     def __fetch_url(self, url: str):
