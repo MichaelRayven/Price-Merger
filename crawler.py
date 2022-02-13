ee51ecd9bb96a59eac015cddbd01318c7a111da6
MichaelRayven <themikerayven@gmail.com>
2022-02-14 01:58:06 +0700

    Added logs as progress indicators

--- crawler.py
+++ crawler.py
@@ -12,13 +12,13 @@ class Crawler():
         self._query = urllib.parse.quote_plus(re.sub(" ", "+", query))
         self._data = []
 
-    @property
-    def urls_list(self, count: int):
+    def list_urls(self, count: int):
         if len(self._data) >= count: self._data[0:count]
         
         self._driver = self.__init_driver()
         with self._driver:
             try:
+                print("[INFO] Getting URLs!")
                 self.__get_urls(count)
             except Exception as err:
                 print(err)
@@ -29,29 +29,36 @@ class Crawler():
         link_elements = self.__recover_progress()
         while len(self._data) < count:
             for link in link_elements:
-                self._data.append(link.get_attribute("href"))
+                url = link.get_attribute("href")
+                print(f"[INFO] Adding: {url} to the list...")
+                self._data.append(url)
                 
             next = self.__next_page()
             if not next: break
             
             link_elements = self._driver.find_elements(By.CSS_SELECTOR, ".model-short-title.no-u")
             if len(link_elements) == 0: break
+        print("[INFO] Collected URLs!")
 
     def __init_driver(self):
         driver = webdriver.Chrome()
         driver.implicitly_wait(5)
         driver.get(f"https://www.e-katalog.ru/ek-list.php?search_={self._query}")
-        WebDriverWait(self._driver, 10).until_not(
+        WebDriverWait(driver, 10).until_not(
             EC.url_to_be(f"https://www.e-katalog.ru/ek-list.php?search_={self._query}"))
+        print("[INFO] Driver initialized!")
         return driver
 
     def __next_page(self):
-        button_element = self.driver.find_element(
+        print("[INFO] Getting next page...", end=" ")
+        button_element = self._driver.find_element(
             By.CSS_SELECTOR, ".ib.select+a")
         if button_element is not None:
             button_element.click()
+            print("Success!")
             return True
         else:
+            print("Fail.")
             return False
 
     def __recover_progress(self):
@@ -68,7 +75,7 @@ class Crawler():
             else:
                 return []
 
-            self._driver.get(self.driver.current_url + f"&page_={page}")
+            self._driver.get(self._driver.current_url + f"&page_={page}")
             links_on_current_page = self._driver.find_elements(
                 By.CSS_SELECTOR, ".model-short-title.no-u")
 
