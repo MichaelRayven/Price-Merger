908e5060b40e49eea760ee09b3a8ccd64c04bfa6
MichaelRayven <themikerayven@gmail.com>
2022-02-13 11:41:16 +0700

    Added some assertions to prevent  crashes

--- parser.py
+++ parser.py
@@ -7,29 +7,33 @@ from bs4 import BeautifulSoup
 class Parser:
     def __init__(self, url_list: list) -> None:
         self.urls = url_list
-        self.data = []
 
-    def parse_data(self):
-        for url in self.urls[len(self.data):]:
-            html_doc = self.__fetch_url(url)
-            page_data = self.__get_page_data(html_doc)
-            if page_data is None: continue
-            self.data.append(page_data)
-        return self.data
+    def parse_data(self) -> list:
+        data = []
+        for url in self.urls:
+            (status, html) = self.__fetch_url(url)
+            if not status: continue
+            (status, data) = self.__get_page_data(html)
+            if not status: continue
+            data.append(data)
+        return data
 
     def __get_page_data(self, html_doc):
         soup = BeautifulSoup(html_doc, "html.parser")
-        try: 
-            script_tag = soup.select(".tabs-content>div script")[1]
-        except Exception:
-            return None
-        code = script_tag.getText()
-        matches = re.findall(r"\"data\":(\[.*\])", code, re.MULTILINE)
-        return json.loads(matches[0])
+        script_tags = soup.select(".tabs-content>div script")
+        if script_tags is None or len(script_tags) < 2: return (False, None)
+        
+        script_text = script_tags[1].getText()
+        match = re.search(r"\"data\":(\[.*\])", script_text, re.MULTILINE)
+        if match is None: return (False, None)
+        
+        data = json.loads(match.group())
+        return (True, data)
 
     def __fetch_url(self, url: str):
         req = requests.get(url)
         if 200 >= req.status_code < 300:
-            return req.content
+            return (True, req.content)
         else:
-            return ""
+            return (False, None)
+    
